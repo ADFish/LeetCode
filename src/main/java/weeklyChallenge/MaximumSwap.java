c0e1c3d07c42d20ffcb8afc2251fe342f8b170e6
duan <duan@miubiq.cs.titech.ac.jp>
2019-01-07 18:16:25 +0900

    add question for maximumSwap

---
+++ src/main/java/weeklyChallenge/MaximumSwap.java
@@ -0,0 +1,27 @@
+package weeklyChallenge;
+
+/**
+ * Given a non-negative integer, you could swap two digits at most once to get the maximum valued number.
+ * Return the maximum valued number you could get.
+ *
+ * Example 1:
+ * Input: 2736
+ * Output: 7236
+ * Explanation: Swap the number 2 and the number 7.
+ *
+ * Example 2:
+ * Input: 9973
+ * Output: 9973
+ * Explanation: No swap.
+ *
+ * @see <a href="https://www.lintcode.com/problem/maximum-swap/description/">Original Problem</>
+ */
+public class MaximumSwap {
+  public static void main(String[] args) {
+
+  }
+
+  public static int maximumSwap(int num) {
+    // Write your code here
+  }
+}
