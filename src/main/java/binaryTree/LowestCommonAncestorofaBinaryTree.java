dfe120b25f3cf35c0532a5868e595e5ea1712ac0
duan <duan@miubiq.cs.titech.ac.jp>
2019-01-15 19:07:08 +0900

    Add question for LowestCommonAncestorofaBinaryTree

---
+++ src/main/java/binaryTree/LowestCommonAncestorofaBinaryTree.java
@@ -0,0 +1,15 @@
+package binaryTree;
+
+/**
+ * Given the root and two nodes in a Binary Tree. Find the lowest common ancestor(LCA) of the two nodes.
+ * The lowest common ancestor is the node with largest depth which is the ancestor of both nodes.
+ *
+ * @see <a href="https://www.lintcode.com/problem/lowest-common-ancestor-of-a-binary-tree/description">Original problem</a>
+ */
+public class LowestCommonAncestorofaBinaryTree {
+
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode A, TreeNode B) {
+    // write your code here
+
+  }
+}
