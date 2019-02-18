ade569a0501839557f4667283e1e87ebe9c1ffaf
duan <duan@miubiq.cs.titech.ac.jp>
2019-02-18 23:33:19 +0900

    Add answer for line web test

---
+++ src/main/java/aainterview/line/SevenTerm/InverseFibonacci.java
@@ -0,0 +1,27 @@
+package aainterview.line.SevenTerm;
+
+public class InverseFibonacci {
+  public static void main(String[] args) {
+    long n = 4;
+    if (n <= 1) {
+      System.out.println(n);
+    }
+
+    long a = 0, b = 1, c = 1;
+    long res = 1;
+
+    while (c < n) {
+      c = a + b;
+      res++;
+      a = b;
+      b = c;
+    }
+
+    if (c != n) {
+      System.out.println(-1);
+    } else {
+      System.out.println(res);
+    }
+
+  }
+}
